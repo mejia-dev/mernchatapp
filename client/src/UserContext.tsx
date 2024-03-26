db66305c485da880085103a8f50cf0f897f2896a
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-26 15:40:07 -0700

    Update UserContext to set Id and Username

--- client/src/UserContext.tsx
+++ client/src/UserContext.tsx
@@ -1,10 +1,17 @@
-import { createContext, useState } from "react";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
 
 export const UserContext: React.Context<{}> = createContext({});
 
 export function UserContextProvider({children}: any) {
   const [username, setUsername] = useState(null);
   const [id, setId] = useState(null);
+  useEffect(() => {
+    axios.get('/profile').then(response => {
+      setId(response.data.userId);
+      setUsername(response.data.username);
+    })
+  }, [])
   return (
     <UserContext.Provider value={{username, setUsername, id, setId}}>{children}</UserContext.Provider>
   )
