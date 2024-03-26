2db4f493663480baa537b92de5db114bda7d5dbf
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-26 14:06:43 -0700

    Update Register form

--- client/src/Register.tsx
+++ client/src/Register.tsx
@@ -1,12 +1,18 @@
+import axios from "axios";
 import { useState } from "react";
 
 export default function Register() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  async function register(e: any) {
+    e.preventventDefault();
+    await axios.post('register', {username, password});
+  }
+
   return (
     <div className="bg-blue-50 h-screen flex items-center">
-      <form className="w-64 mx-auto mb-12">
+      <form className="w-64 mx-auto mb-12" onSubmit={register}>
         <input 
           value={username} 
           onChange={e => setUsername(e.target.value)} 
