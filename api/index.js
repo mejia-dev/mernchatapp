bfa38769320c4691c4408d4dda6cc3f7c251f86f
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-26 13:46:59 -0700

    Add register route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const User = require('./models/User');
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL);
@@ -11,8 +12,10 @@ app.get('/test', (req,res) => {
   res.json('test ok');
 })
 
-app.post('/register', (req,res) => {
-  
+app.post('/register', async (req,res) => {
+  const {username, password} = req.body;
+  await User.create({username,password});
+  res.json();
 })
 
 app.listen(4040);
