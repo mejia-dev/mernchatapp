a4a9e6aa7bc2f2edf6866969084f216f96583857
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-26 13:39:43 -0700

    Use dotenv

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 
 const app = express();
 
 app.get('/test', (req,res) => {
   res.json('test ok');
+})
+
+app.post('/register', (req,res) => {
+  
 })
 
 app.listen(4040);
