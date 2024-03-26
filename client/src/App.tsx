19f3b5650724decc81d860a23f849a6b0c4d108e
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-26 14:46:07 -0700

    Wrap Routes in context to ensure context is being passed correctly

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import axios from "axios"
-import Register from "./Register"
 import { UserContextProvider } from "./UserContext";
+import Routes from "./Routes";
 
 function App() {
   axios.defaults.baseURL = 'http://localhost:4040';
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <UserContextProvider>
-        <Register />
+        <Routes />
       </UserContextProvider>
     </>
   )
---
+++ client/src/Routes.tsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import Register from "./Register";
+import { UserContext } from "./UserContext";
+
+export default function Routes() {
+  const {username, id}: any = useContext(UserContext);
+
+  if (username) {
+    return 'logged in!'
+  }
+
+  return (
+    <Register />
+  )
+}
