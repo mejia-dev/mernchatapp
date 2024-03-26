e824193faec84b6fd5704abf6b5ecd8514337215
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-26 13:52:44 -0700

    Update register route to return jwt

--- api/index.js
+++ api/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const User = require('./models/User');
+const jwt = require('jsonwebtoken');
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL);
+const jwtSecret = process.env.JWT_SECRET;
 
 const app = express();
 
@@ -14,8 +16,11 @@ app.get('/test', (req,res) => {
 
 app.post('/register', async (req,res) => {
   const {username, password} = req.body;
-  await User.create({username,password});
-  res.json();
+  const createdUser = await User.create({username,password});
+  jwt.sign({userId:createdUser._id}, jwtSecret, (err, token) => {
+    if (err) throw err;
+    res.cookie('token', token).status(201).json('ok');
+  });
 })
 
 app.listen(4040);
