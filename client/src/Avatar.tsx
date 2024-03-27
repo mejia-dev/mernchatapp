15691d4d14af7a4837a0020eb795e17b75131cac
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-27 13:36:46 -0700

    Remove console logging

--- client/src/Avatar.tsx
+++ client/src/Avatar.tsx
@@ -1,9 +1,7 @@
 export default function Avatar({ username, userId }: { username: string, userId: string }) {
   const colorList: string[] = ["bg-red-200", "bg-orange-200", "bg-amber-200", "bg-yellow-200", "bg-lime-200", "bg-green-200", "bg-emerald-200", "bg-teal-200", "bg-cyan-200", "bg-sky-200", "bg-blue-200", "bg-violet-200", "bg-purple-200", "bg-stone-200", ];
   const avatarColorIndex: number = parseInt(userId, 16) % colorList.length;
-  console.log(avatarColorIndex)
   const avatarColor: string = colorList[avatarColorIndex];
-  console.log(avatarColor)
   return (
     <div className={"w-8 h-8 rounded-full flex items-center " + avatarColor}>
       <div className="text-center w-full opacity-70">{username[0]}</div>
