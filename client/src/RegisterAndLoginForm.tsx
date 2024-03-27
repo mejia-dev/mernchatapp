a431802d216030c781e1178732ba6765b47df32f
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-27 12:04:23 -0700

    Add autocomplete attributes for form

--- client/src/RegisterAndLoginForm.tsx
+++ client/src/RegisterAndLoginForm.tsx
@@ -34,12 +34,15 @@ export default function Register() {
           onChange={e => setUsername(e.target.value)}
           type="text"
           placeholder="username"
-          className="block w-full rounded-sm p-2 mb-2 border" />
+          autoComplete="username"
+          className="block w-full rounded-sm p-2 mb-2 border"
+        />
         <input
           value={password}
           onChange={e => setPassword(e.target.value)}
           type="password"
           placeholder="password"
+          autoComplete="current-password"
           className="block w-full rounded-sm p-2 mb-2 border" />
         <button className="bg-blue-500 text-white block w-full rounded-sm p-2 border" type="submit">
           {isLoginOrRegister}
