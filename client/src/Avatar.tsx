d7fb4aaf187a9a013f39c26527e71f8af234af06
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-28 16:47:20 -0700

    Add status indicator colors

--- client/src/Avatar.tsx
+++ client/src/Avatar.tsx
@@ -1,14 +1,12 @@
 export default function Avatar({ username, userId, online }: { username: string, userId: string, online: boolean }) {
-  const colorList: string[] = ["bg-red-200", "bg-orange-200", "bg-amber-200", "bg-yellow-200", "bg-lime-200", "bg-green-200", "bg-emerald-200", "bg-teal-200", "bg-cyan-200", "bg-sky-200", "bg-blue-200", "bg-violet-200", "bg-purple-200", "bg-stone-200", ];
+  const colorList: string[] = ["bg-red-200", "bg-orange-200", "bg-amber-200", "bg-yellow-200", "bg-lime-200", "bg-green-200", "bg-emerald-200", "bg-teal-200", "bg-cyan-200", "bg-sky-200", "bg-blue-200", "bg-violet-200", "bg-purple-200", "bg-stone-200",];
   const avatarColorIndex: number = parseInt(userId, 16) % colorList.length;
   const avatarColor: string = colorList[avatarColorIndex];
+  const statusIndicator: string = online ? "bg-green-400" : "bg-gray-400";
   return (
     <div className={"w-8 h-8 relative rounded-full flex items-center " + avatarColor}>
       <div className="text-center w-full opacity-70">{username[0]}</div>
-      {online && (
-        <div className="absolute w-3 h-3 bg-green-400 -bottom-0.5 -right-0.5 rounded-full border border-whit"></div>
-      )}
-      
+      <div className={"absolute w-3 h-3 -bottom-0.5 -right-0.5 rounded-full border border-white " + statusIndicator} />
     </div>
   )
 }
